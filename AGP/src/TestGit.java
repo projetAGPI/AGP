982eab1c15211016cead020586e6207298deb8eb
aissam bounoua <aissam.bounoua@gmail.com>
2017-01-16 10:10:42 +0100

    test de git

---
+++ AGP/src/TestGit.java
@@ -0,0 +1,35 @@
+
+public class TestGit {
+	
+	public int value1;
+	public int value4;
+	public int value3;
+	public int value2;
+	
+	public int getValue1() {
+		return value1;
+	}
+	public void setValue1(int value1) {
+		this.value1 = value1;
+	}
+	public int getValue4() {
+		return value4;
+	}
+	public void setValue4(int value4) {
+		this.value4 = value4;
+	}
+	public int getValue3() {
+		return value3;
+	}
+	public void setValue3(int value3) {
+		this.value3 = value3;
+	}
+	public int getValue2() {
+		return value2;
+	}
+	public void setValue2(int value2) {
+		this.value2 = value2;
+	}
+	
+
+}
